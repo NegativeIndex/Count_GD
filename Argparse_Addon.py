32ed5fd04c11345eeb1dadd3ad0b7eab606c38d2
NegativeIndex <daiweitao@yahoo.com>
2019-12-08 16:33:09 -0600

    Argparse_Addon can process maxdepth now

--- Argparse_Addon.py
+++ Argparse_Addon.py
@@ -3,8 +3,7 @@
 Some imporvements over the optparse module. The first part is to process unknown options; the second part is to process single dash long name.
 """
 
-import argparse 
-import logging
+import argparse, logging
 import sys,subprocess,os,re
 
 ############################
@@ -32,77 +31,129 @@ def long_option_dash_2to1(args):
         new_argv.append(arg)
     return new_argv
 
+# modify help string
+def modify_help_string(help_string,longnames=None,sentences=None):
+    """Modify the help_string. It receive a help_string, changes the double
+    dash in front of some names to single dash, and add some sentences
+    at the end.
+
+    Parameters
+    -----------
+    help_string : string
+      the string return by parser.format_help()
+    longnames: string list
+      The names with single dash
+    sentences: string list
+      The sentences added at the end
+
+    Returns
+    -----------
+    string
+      The new version of help string
+
+    """
+    if longnames is not None:
+        for name in longnames:
+            st1='--'+name
+            st2='-'+name
+            help_string=help_string.replace(st1, st2)
+
+    if sentences is not None:    
+        help_string.rstrip()
+        help_string=help_string[:-1]
+        for line in sentences:
+            help_string+='\n'+line
+            help_string+='\n'
+
+    return help_string
+
 
 #########################
 # main function
 def main(argv):
-    """A main function to implement the enhanced optparse_addon
-    function. The function try to find job.info file in all the
-    folders. The unknown options are passed to linux bash find
-    function.
+    """A main function to demonstrate the funciton argparse_addon
+    function. The function try to find some files and process them.
+    The unknown options are passed to linux bash find function.
 
     I want to enforce a default vaule to -maxdepth option. I have to
     intercept the parser's help message.
+
+    Usage: Argparse_Addon.py [-p [PATH [PATH ...]]] [-s] [-maxdepth MAXDEPTH] [-h]
+
+    optional arguments:
+  
+      -p [PATH [PATH ...]], --path [PATH [PATH ...]]     
+                           Searching paths
+
+      -s, --summary         Demonstrate only summary
+
+      -h, --help            Show this message and quit
+
+    option modification:
+    
+      ``-maxdepth MAXDEPTH``    
+          maximum search depth, default is 1
+
+      All other long names with single dash will be passed to bash find command.
 
     """
     # logger
     logging.basicConfig(level=logging.DEBUG,
                         format='%(levelname)s: %(message)s')
 
-    str1='Usage: %prog <dir> '
-    str2='-maxdepth <int> '
-    str4='This function try to find job.info files and process them.'
+    str4='This function try to find some files and process them.'
     str5='It passes the unknown options to find function.'
-    parser = PassThroughOptionParser(usage=str1+str2,
-                                     description=str4+str5,
-                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    # parser = OptionParser()
-    # convert args help 
-    # if args.help:
-    #     help_string = parser.format_help()
-    #     print(help_string.replace('--', '-'))
-    #     parser.exit(0)
+ 
+    # intercept help
+    parser=argparse.ArgumentParser(add_help=False,
+                                   description='\n'.join((str4,str5)) )
+    # parser=argparse.ArgumentParser(description='\n'.join((str4,str5)) )
 
     # add some options
-    parser.add_option("-u", "--unfinished",
-                      action="store_true",
-                      default=False,
-                      help="Demonstrate unfinished folders.")
+    parser.add_argument("-p","--path",
+                      default=['./',], nargs='*',
+                      help="Searching paths")
 
-    parser.add_option("-s", "--summary",
+
+    parser.add_argument("-s", "--summary",
                       action="store_true",
                       default=False,
                       help="Demonstrate only summary.")
 
-    parser.add_option("-c", "--count",type=int, default=0,
-                      help="Demonstrate the longest jobs.")
-
-
-    parser.add_option("--maxdepth",type=int, default=1,
+    parser.add_argument("--maxdepth",type=int, default=1,
                       help="maximum search depth, default is 1")
 
+    parser.add_argument('-h', '--help', 
+                        help='Show this message and quit', 
+                        action='count')
 
+    ## process unknown options
+    argv.pop(0)  # remove the first, command name
+    # logging.debug("Before process are {}".format(argv))
     sargs=long_option_dash_1to2(argv)
-    (opts,unk_args) = parser.parse_args(sargs)
+    args,unk_args=parser.parse_known_args(sargs)
     unk_args=long_option_dash_2to1(unk_args)
 
     # process unk_args
-    unk_args.pop(0)  # remove the first, command name
-    logging.debug("Saved args are {}".format(sargs))
-    logging.debug("Accepted options is {}".format(opts))
-    logging.debug("Unkonwn args are {}".format(unk_args))
+    # logging.debug("Saved args are {}".format(sargs))
+    # logging.debug("Accepted args is {}".format(args))
+    # logging.debug("Unkonwn args are {}".format(unk_args))
 
-    # find all the folders which contains simulations
-    comm=["find"]+unk_args+['-name',"job.info"]
+    # new help documents
+    if args.help:
+        help_string = parser.format_help()
+        sentences=('  All other long names with single dash '+
+            'will be passed to bash find command.', )
+        help_string=modify_help_string(help_string,('maxdepth',),
+                                       sentences)
+        print(help_string)
+        parser.exit(0)
+
+    # find files and list them
+    comm=["find"]+args.path+['-maxdepth',str(args.maxdepth)]+unk_args
     res = subprocess.check_output(comm).decode("ascii").rstrip()
-    # logging.debug(res)
-    jobfiles=res.split('\n')
-    jobfiles=[os.path.abspath(jobfile) for jobfile in jobfiles 
-              if len(jobfile.strip())>0]
-
-    jobfiles.sort()
-    for f in jobfiles:
-        logging.debug(f)
+    print(res)
+  
 
 
 #########################
